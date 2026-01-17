eac909605c3a262da4dcb64530156fe0dfebcfb3
mardinw <mardinw@outlook.com>
2026-01-18 01:20:12 +0700

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,9 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <meta name="apple-mobile-web-app-title" content="MaoMao" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/web-app-manifest-192x192.png" />
+        <link rel="icon" type="image/png" href="/web-app-manifest-192.png" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@500;600;700&display=swap" rel="stylesheet" />
         {settings?.primaryColor && (
           <style
